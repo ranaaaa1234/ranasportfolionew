a17a870a76615fedaffad57c6a2e75391e8672d1
ranaSafi <ranaa_safi@hotmail.com>
2025-07-06 16:18:29 +0200

    testing

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,9 @@
 import { CalendarHeart, Users, Github, ExternalLink, Ban } from "lucide-react";
 import Tooltip from "./Tooltip";
-import imgPortfolio from "/public/assets/portfolio.jpeg";
-import imgChatify from "/public/assets/chatifyImg.jpeg";
-import imgFood from "/public/assets/foodImg.jpeg";
-import imgWeather from "/public/assets/weatherAppImg.jpeg";
+import imgPortfolio from "/assets/portfolio.jpeg";
+import imgChatify from "/assets/chatifyImg.jpeg";
+import imgFood from "/assets/foodImg.jpeg";
+import imgWeather from "/assets/weatherAppImg.jpeg";
 
 function Projects() {
   const projectList = [
