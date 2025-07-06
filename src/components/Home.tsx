73082851fa13f3ef2dbb4048a6518cacb137bd52
ranaSafi <ranaa_safi@hotmail.com>
2025-07-06 12:26:32 +0200

    added cpontact links to home and tooltip props

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,13 @@
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import { Mail, Github, Linkedin } from "lucide-react";
+import Tooltip from "./Tooltip";
 
 function Home() {
   const handleScroll = () => {
     const section = document.getElementById("about");
     section?.scrollIntoView({ behavior: "smooth" });
   };
+  const email = "ranaa_safi@hotmail.com";
 
   return (
     <section
@@ -18,8 +21,44 @@ function Home() {
         <p className="mt-4 xs:text-3xl  lg:text-4xl  text-blue-600 max-w-sm xs:max-w-xs">
           Frontend developer & UX/UI-designer
         </p>
+
+        <div className="flex flex-row items-center gap-6 py-10 px-4 lg:px-2">
+          <Tooltip text="Email">
+            <a
+              href={`mailto:${email}`}
+              target="_blank"
+              rel="nooper noreferrer"
+              className="flex items-center gap-3 border border-blue-400 text-blue-900 rounded-lg p-3 shadow-sm hover:bg-gradient-to-br from-blue-50 via-purple-100 to-blue-200"
+            >
+              <Mail className="w-7 h-7" />
+            </a>
+          </Tooltip>
+
+          <Tooltip text="Github">
+            <a
+              href="https://github.com/ranaaaa1234"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-3 border border-blue-400 text-blue-900 rounded-lg p-3 shadow-sm hover:bg-gradient-to-br from-blue-50 via-purple-100 to-blue-200"
+            >
+              <Github className="w-7 h-7" />
+            </a>
+          </Tooltip>
+
+          <Tooltip text="LinkedIn">
+            <a
+              href="https://www.linkedin.com/in/ranna-safi/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-3 border border-blue-400 text-blue-900 rounded-lg p-3 shadow-sm hover:bg-gradient-to-br from-blue-50 via-purple-100 to-blue-200"
+            >
+              <Linkedin className="w-7 h-7" />
+            </a>
+          </Tooltip>
+        </div>
+
         <div>
-          <p className="mt-20 sm:text-lg md:text-xl lg:text-2xl text-blue-900 flex items-center justify-center">
+          <p className=" sm:text-lg md:text-xl lg:text-2xl text-blue-900 flex items-center justify-center">
             Explore my portfolio to see my work and skills.
           </p>
         </div>
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type tooltipProps = {
+  text: string;
+  children: React.ReactNode;
+};
+
+const Tooltip: React.FC<tooltipProps> = ({ text, children }) => {
+  return (
+    <div className="relative group inline-block">
+      {children}
+      <span
+        className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 
+                       opacity-0 group-hover:opacity-100 bg-blue-900 text-white text-xs 
+                       rounded px-2 py-1 transition duration-200 z-50 whitespace-nowrap"
+      >
+        {text}
+      </span>
+    </div>
+  );
+};
+
+export default Tooltip;
